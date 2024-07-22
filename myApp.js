a620b4363284f5ded2f7bc8c76ea558af85f5194
Rajkumar <rajkumarrajkumar061@gmail.com>
2024-07-22 16:40:14 +0000

    __dirname

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-app.get("/", (req, res) => res.send("Hello Express"))
+// app.get("/", (req, res) => res.send("Hello Express"))
 
 console.log(__dirname + "/views/index.html");
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"))
