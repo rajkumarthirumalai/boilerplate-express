2e98c63c0dbd45f45268dd17b3ddd219b472c085
Rajkumar <rajkumarrajkumar061@gmail.com>
2024-07-23 17:35:03 +0000

    time

--- myApp.js
+++ myApp.js
@@ -5,16 +5,21 @@ let app = express();
 console.log("Hello World");
 
 app.use("/public", express.static(path.join(__dirname, "public")))
-app.use((req, res, next) => { 
+app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`);
     next();
- })
+})
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "index.html")))
 app.get("/json", (req, res) => {
     console.log(process.env.MESSAGE_STYLE);
     process.env.MESSAGE_STYLE == "uppercase" ? res.json({ "message": "HELLO JSON" }) :
         res.json({ "message": "Hello json" })
 })
+app.get(
+    "/time",
+     (req, res, next) => { req.time = new Date().toString(); next(); },
+     (req, res) => {console.log(req.time); res.json({time:req.time})}
+    )
 
 
 
