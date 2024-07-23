437ccaa15da4e8e6237624f754d061c63d8029ff
Rajkumar <rajkumarrajkumar061@gmail.com>
2024-07-23 16:33:27 +0000

    env

--- myApp.js
+++ myApp.js
@@ -1,9 +1,17 @@
+require('dotenv').config()
 let express = require('express');
 let path = require('path');
 let app = express();
 console.log("Hello World");
-app.use("/public", express.static(path.join(__dirname , "public")))
-app.get("/", (req, res) => res.sendFile(path.join(__dirname,"views","index.html")))
+
+app.use("/public", express.static(path.join(__dirname, "public")))
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "index.html")))
+app.get("/json", (req, res) => {
+    console.log(process.env.MESSAGE_STYLE);
+    process.env.MESSAGE_STYLE == "uppercase" ? res.json({ "message": "HELLO JSON" }) :
+        res.json({ "message": "Hello json" })
+})
+
 
 
 
