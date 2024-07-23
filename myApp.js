ec0ab6cb6b4d9f01e69be27bdd98f4fd62f0afe5
Rajkumar <rajkumarrajkumar061@gmail.com>
2024-07-23 16:59:16 +0000

    middleware

--- myApp.js
+++ myApp.js
@@ -5,6 +5,10 @@ let app = express();
 console.log("Hello World");
 
 app.use("/public", express.static(path.join(__dirname, "public")))
+app.use((req, res, next) => { 
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+ })
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "index.html")))
 app.get("/json", (req, res) => {
     console.log(process.env.MESSAGE_STYLE);
